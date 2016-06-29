431169c2e2afeeef68ee5ee3ee4deb3488e33a8d
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-06-29 17:07:56 +0530

    route updated

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,4 @@ var UserController = require('./../controllers/UserCtrl');
 /* GET users listing. */
 router.get('/', UserController.sayHi);
 
-/* Sample DB use */
-router.get('/isactive/:uid', UserController.randomCheck);
-
 module.exports = router;
