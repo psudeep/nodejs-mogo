6aae095212641bdeac8f5911918cba0a55fbebd0
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-06-29 16:34:55 +0530

    routes

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+/*
+@Company: Medimojo
+@Author: Prashant Sudeep
+*/
+
+var express = require('express');
+var router = express.Router();
+var UserController = require('./../controllers/UserCtrl');
+
+/* GET users listing. */
+router.get('/', UserController.sayHi);
+
+/* Sample DB use */
+router.get('/isactive/:uid', UserController.randomCheck);
+
+module.exports = router;
