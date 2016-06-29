17817284836e3bb8684549ec2553cf5aa943b620
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-06-29 17:07:10 +0530

    mongoconnect

--- models/UsersUtils.js
+++ models/UsersUtils.js
@@ -3,22 +3,5 @@
 @Author: Prashant Sudeep
 */
 
-var sqldb = require('./MongoConnector');
+var mongodb = require('./MongoConnector');
 
-exports.getUserDetails = function(uid, cb) {
-    var query = "SELECT name, address FROM users WHERE id='"+uid+"' LIMIT 1";
-    sqldb.queryDB(query, cb);
-};
-
-exports.isUserActive = function(uid, cb) {
-    var query = "SELECT active FROM users WHERE id='"+uid+"' LIMIT 1";
-    sqldb.queryDB(query, function(err, rows) {
-        if(err || ! rows.length) {
-            cb(err, false);
-        }
-        else {
-            rows = rows[0];
-            cb(null, rows.active === 1);
-        }
-    });
-};
