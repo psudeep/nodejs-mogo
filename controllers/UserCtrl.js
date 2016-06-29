f2c5289a68012b460bf5b24d52582caf480c9cef
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-06-29 17:04:01 +0530

    save Data function added

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -4,6 +4,7 @@
 */
 
 var UserModel = require('./../models/UsersUtils');
+var MongooseModel = require('./../models/MongooseConnect');
 
 exports.sayHi = function(req, res, next) {
     res.send('Howdy from User Controller!');
@@ -29,3 +30,22 @@ exports.randomCheck = function(req, res, next) {
         res.status(400).json({err: true, msg: "User id missing from url."});
     }
 };
+
+exports.saveData = function(req, res, next){
+    var name = req.query.name;
+    var email = req.query.email;
+    var mobile = req.query.mobile;
+    if(name=="" || email=="" || mobile==""){
+        res.status(400).json({err: true, msg: "missing name, email & mobile in query string"});
+    }else{
+        var data = {"name": name, "email": email, "mobile": mobile};
+        MongooseModel.InsertSingle(data, function(err,results){
+            if(err){
+                res.status(400).json({err: true, msg: err});
+            }else{
+                res.status(200).json({err: false, msg: "Data Saved ! "});
+            }
+        })
+    }
+
+}
