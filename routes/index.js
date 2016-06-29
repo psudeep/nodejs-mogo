94887b943e064c8497b05622753d21e495d3d27d
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-06-29 17:04:20 +0530

    new routes for saving data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var UserController = require('./../controllers/UserCtrl');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MongoConnect' });
 });
+
+router.get('/addUser', UserController.saveData);
 
 module.exports = router;
